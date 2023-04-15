3a77f71597c341bb9370c29a725d2ed57752eaf0
James Dunn <james.ed.dunn@gmail.com>
2023-04-15 22:43:46 +0900

    Init commit

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,28 @@
+
+    <script>
+    
+  import { onMount } from "svelte";
+  import { isReady, Mina, PublicKey } from 'snarkyjs';
+
+  onMount(async () => {
+    await isReady;  
+
+    const { Add } = await import('../../../contracts/build/src/')
+    // Update this to use the address (public key) for your zkApp account
+    // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
+    // Berkeley Testnet B62qisn669bZqsh8yMWkNyCA7RvjrL6gfdr3TQxymDHNhTc97xE5kNV
+    const zkAppAddress = ''
+    // This should be removed once the zkAppAddress is updated.
+    if (!zkAppAddress) {
+      console.error(
+        'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qqkb7hD1We6gEfrcqosKt9C398VLp1WXeTo1i9boPoqF7B1LxHg4',
+      );
+    }
+    const zkApp = new Add(PublicKey.fromBase58(zkAppAddress))
+  });
+
+    </script>
+
+    <h1>
+    Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
